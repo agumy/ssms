ac1a1a3303e42c9c4d4914fc2eb7ebf37c42b12f
agumy <nov13sh@gmail.com>
2021-08-25 20:58:13 +0900

    use summoner component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { Summoner } from "../components/summoner";
+
+const Positions = ["top", "jungle", "mid", "bot", "support"] as const;
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Box p="2" bg="background.base" color="white">
+      <Flex direction="column" h="full" align="flex-start" justify="flex-start">
+        {Positions.map((pos) => (
+          <>
+            <Summoner key={pos} position={pos} />
+            <div style={{ height: "8px" }} />
+          </>
+        ))}
+      </Flex>
+    </Box>
+  );
 }
